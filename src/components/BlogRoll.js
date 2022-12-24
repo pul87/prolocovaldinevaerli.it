87bd5e468b235a58596b8a99d46a3222be973e69
Paolo Basso <paolo.basso87@gmail.com>
2022-12-24 19:31:47 +0100

    Aumentato numero di ultime notizie da 3 a 10

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -75,7 +75,7 @@ export default () => (
         allMarkdownRemark(
           sort: { order: DESC, fields: [frontmatter___date] }
           filter: { frontmatter: { templateKey: { eq: "blog-post" }, published: {eq: true} } }
-          limit: 3
+          limit: 10
         ) {
           edges {
             node {
